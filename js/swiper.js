a73848dc6366a24354691a8cb77e069067097298
Sabrina1312 <sabrinka1312@gmail.com>
2026-05-13 04:41:48 +0300

    fix(reviews): background-image slides

--- js/swiper.js
+++ js/swiper.js
@@ -12,19 +12,19 @@ const swiper = new Swiper(".reviews__swiper", {
       slidesPerView: 1.05,
       spaceBetween: 12,
     },
-    520: {
+    630: {
       slidesPerView: 1.7,
       spaceBetween: 20,
     },
-    650: {
+    760: {
       slidesPerView: 2.05,
       spaceBetween: 16,
     },
-    769: {
+    900: {
       slidesPerView: 2.2,
       spaceBetween: 16,
     },
-    900: {
+    1360: {
       slidesPerView: 3.05,
       spaceBetween: 20,
     },
